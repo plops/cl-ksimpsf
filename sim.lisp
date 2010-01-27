7ba05f47b43e57ffc2f45a0fc8c7e7009d2f5b70
mr chickenfarmer <martin@torben.(none)>
2010-01-27 08:24:23 +0000

    i actually got the 3d rendering working at home

--- sim.lisp
+++ sim.lisp
@@ -52,31 +52,59 @@
       0f0
       (log x)))
 
-
+(defun clamp (x)
+  (cond ((< x 0) 0)
+	((<= x 255) x)
+	(t 255)))
 
 (defmethod glut:display-window :before ((w mk-window))
+  (enable :depth-test :blend :line-smooth)
   (line-width 2)
+  (blend-func :src-alpha :one-minus-src-alpha)
+;;   (tex-gen :s :texture-gen-mode :object-linear)
+;;   (tex-gen :t :texture-gen-mode :object-linear)
+;;   (tex-gen :r :texture-gen-mode :object-linear)
+;;   (enable :texture-gen-s
+;; 	  :texture-gen-t
+;; 	  :texture-gen-r)
   (let* ((target :texture-3d))  
     (setf (tex w) (first (gen-textures 1)))
     (bind-texture target (tex w))
     (tex-parameter target :texture-mag-filter :linear)
     (tex-parameter target :texture-min-filter :linear)
-    (let* ((psf (sim-psf :size '(128 128 128)))
-	   (buf (make-array (* 128 128 128) :element-type '(unsigned-byte 8)))
-	   (scale (/ 255 (loop for i below (* 128 128 128)
-		      maximize (abs (aref (sb-ext:array-storage-vector psf) i))))))
+    (let* (#+nil (psf (sim-psf :size '(128 128 128)))
+		 (n 64)
+		 (f (/ 256 n))
+		 (buf (make-array (list n n n 4) :element-type '(unsigned-byte 8)))
+		 #+nil (scale (/ 255 (loop for i below (* 128 128 128)
+					   maximize (abs (aref (sb-ext:array-storage-vector psf) i))))))
       
-      (dotimes (i 128)
-	(dotimes (j 128)
-	  (dotimes (k 128)
-	    (setf (aref buf (array-row-major-index psf i j k))
-		  i))))
-    #+nil  (dotimes (i (* 128 128 128))
-	(setf (aref buf i)
-	      (floor (* scale (abs (aref (sb-ext:array-storage-vector psf) i))))))
-      (format t "~a~%" scale)
-      (cffi:with-pointer-to-vector-data (p buf)
-	(tex-image-3d target 0 :rgba 128 128 128 0 :luminance :unsigned-byte p)))))
+      (dotimes (i n)
+	(dotimes (j n)
+	  (dotimes (k n)
+	    (let* ((h (/ n 2))
+		   (s (/ 1d0 n))
+		   (x (* s (- i h)))
+		   (y (* s (- j h)))
+		   (z (* s (- k h)))
+		   (r (+ (* x x) (* y y) (* z z))))
+	      (declare (fixnum i j k n h)
+		       (double-float s x y z r))
+	      (setf (aref buf i j k 0) (* f i)
+		    (aref buf i j k 1) (* f j)
+		    (aref buf i j k 2) (* f k)
+		    (aref buf i j k 3) 
+		    (cond ((< r (* .2 .2)) 128)
+			  ((< r (* .4 .4)) (if (< k h) 
+					       54
+					       5))
+			  (t 0)))))))
+      #+nil  (dotimes (i (* 128 128 128))
+	       (setf (aref buf i)
+		     (floor (* scale (abs (aref (sb-ext:array-storage-vector psf) i))))))
+      
+      (cffi:with-pointer-to-vector-data (p (sb-ext:array-storage-vector buf))
+	(tex-image-3d target 0 :rgba n n n 0 :rgba :unsigned-byte p)))))
 
 (defmethod glut:reshape ((win mk-window) w h)
   (setf (glut:width win) w
@@ -98,54 +126,68 @@
 
 (defun draw-cube ()
   (disable :texture-3d)
-  (with-pushed-matrix
-    (with-primitive :line-strip
-      (vertex 0 0 0)
-      (vertex 1 0 0)
-      (vertex 1 0 1)
-      (vertex 0 0 1)
-      (vertex 0 0 0))
-    (with-primitive :line-strip
-      (vertex 0 1 0)
-      (vertex 1 1 0)
-      (vertex 1 1 1)
-      (vertex 0 1 1)
-      (vertex 0 1 0))
-    (with-primitive :lines
-      (vertex 0 .1 1)
-      (vertex 0 .9 1)
+  (with-primitive :line-strip
+
+    (color 1 0 0)
+    (vertex 0 0 0)
+    (vertex 1 0 0)
+    (color 1 1 1)
+
+    (vertex 1 0 0)
+    (vertex 1 0 1)
+    (vertex 0 0 1)
+    
+    (color 0 0 1)
+    (vertex 0 0 1)
+    (vertex 0 0 0)
+    (color 1 1 1))
+  (with-primitive :line-strip
+    (vertex 0 1 0)
+    (vertex 1 1 0)
+    (vertex 1 1 1)
+    (vertex 0 1 1)
+    (vertex 0 1 0))
+  (with-primitive :lines
+    (vertex 0 .1 1)
+    (vertex 0 .9 1)
       
-      (vertex 1 .1 1)
-      (vertex 1 .9 1)
+    (vertex 1 .1 1)
+    (vertex 1 .9 1)
 
-      (vertex 1 .1 0)
-      (vertex 1 .9 0)
+    (vertex 1 .1 0)
+    (vertex 1 .9 0)
+    
+    (color 0 1 0)
+    (vertex 0 .1 0)
+    (vertex 0 .9 0)
+    (color 1 1 1)))
 
-      (vertex 0 .1 0)
-      (vertex 0 .9 0))))
+(defparameter *rot* 0)
 
 (defun draw ()
   (clear-color 0 0 0 1)
   (clear :color-buffer-bit :depth-buffer-bit)
   (enable :texture-3d)
   (with-pushed-matrix 
-    (with-primitive :quads
-      (vertex 0 0 0)
-      (vertex 1 1 0)
-      (vertex 1 1 1)
-      (vertex 0 0 1))
-    (with-primitive :quads
-      (tex-coord  0 0 .5)
-      (vertex 0 0 .5)
-      (tex-coord 1 0 .5)
-      (vertex 1 0 .5)
-      (tex-coord 1 1 .5)
-      (vertex 1 1 .5)
-      (tex-coord 0 1 .5)
-      (vertex 0 1 .5)))
-  (draw-cube)
-  
-  (glut:swap-buffers))
+    (rotate *rot* 0 0 1)
+    (translate -.5 -.5 -.5)
+    (setf *rot* (if (> *rot* 359)
+		    0
+		    (+ 3 *rot*)))
+    (loop for z upto 1.001 by .02 do
+     (with-primitive :quads
+       (tex-coord 0 0 z)
+       (vertex 0 0 z)
+       (tex-coord 1 0 z)
+       (vertex 1 0 z)
+       (tex-coord 1 1 z)
+       (vertex 1 1 z)
+       (tex-coord 0 1 z)
+       (vertex 0 1 z)))
+    (draw-cube))
+  (glut:swap-buffers)
+  (sleep (/ 34))
+  (glut:post-redisplay))
 
 (defmethod glut:display ((w mk-window))
   (format t "display~%")
@@ -157,7 +199,7 @@
 
 (defun run ()
   (glut:display-window (make-instance 'mk-window)))
-#+nil
+;#+nil
 (run)
 
 
